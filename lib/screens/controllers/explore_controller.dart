ef27b95ebdade3bcd883716942ca7faab17ec84e
nayeemtby <nayeemtby@gmail.com>
2022-01-12 23:46:31 +0600

    Add animation

--- lib/screens/controllers/explore_controller.dart
+++ lib/screens/controllers/explore_controller.dart
@@ -1,3 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class ExploreController extends GetxController {}
+class ExploreController extends GetxController {
+  RxBool showSearch = false.obs;
+  RxBool isSearchBarFocused = false.obs;
+  TextEditingController searchTextController = TextEditingController();
+  late AnimationController titleAnimationController;
+  late AnimationController iconAnimationController;
+  void hideSearch() {
+    searchTextController.clear();
+    showSearch.value = false;
+  }
+
+  void searchFocusChanged(bool focused) {
+    isSearchBarFocused.value = focused;
+    if (focused) {
+      iconAnimationController.animateTo(1);
+      titleAnimationController.animateTo(0);
+    } else {
+      iconAnimationController.animateTo(0);
+      titleAnimationController.animateTo(1);
+    }
+    shouldShowSearch();
+  }
+
+  void shouldShowSearch() {
+    if (isSearchBarFocused.value || searchTextController.text.isNotEmpty) {
+      showSearch.value = true;
+    } else {
+      showSearch.value = false;
+    }
+  }
+}
--- lib/screens/explore_page.dart
+++ lib/screens/explore_page.dart
@@ -1,22 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:grocer_client/screens/category.dart';
 import 'package:grocer_client/screens/components/explore_components.dart';
+import 'package:grocer_client/screens/controllers/explore_controller.dart';
 import 'package:grocer_client/theme/colors.dart';
 import 'package:grocer_client/theme/txttheme.dart';
 
 class ExplorePage extends StatefulWidget {
-  const ExplorePage({Key? key}) : super(key: key);
+  ExplorePage({Key? key}) : super(key: key);
+
+  final ExploreController exploreController = Get.isRegistered()
+      ? Get.find()
+      : Get.put(
+          ExploreController(),
+        );
 
   @override
   State<ExplorePage> createState() => _ExplorePageState();
 }
 
 class _ExplorePageState extends State<ExplorePage>
-    with AutomaticKeepAliveClientMixin {
+    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
   @override
   bool get wantKeepAlive => true;
+  late final AnimationController titleAnimationController;
+  late final AnimationController iconAnimationController;
+  @override
+  void initState() {
+    super.initState();
+    titleAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 300,
+      ),
+    );
+    iconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 300,
+      ),
+    );
+    widget.exploreController.titleAnimationController =
+        titleAnimationController;
+    widget.exploreController.iconAnimationController = iconAnimationController;
+    titleAnimationController.animateTo(
+      1,
+      duration: const Duration(
+        milliseconds: 0,
+      ),
+    );
+    iconAnimationController.animateTo(
+      0,
+      duration: const Duration(
+        milliseconds: 0,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,20 +71,59 @@ class _ExplorePageState extends State<ExplorePage>
       ),
       child: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 16.r,
-            ),
-            child: Text(
-              'Explore Products',
-              style: TxtThemes.headline,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizeTransition(
+                sizeFactor: CurvedAnimation(
+                  parent: titleAnimationController,
+                  curve: Curves.ease,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.r,
+                  ),
+                  child: Text(
+                    'Explore Products',
+                    style: TxtThemes.headline,
+                  ),
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: EdgeInsets.symmetric(
               vertical: 14.r,
             ),
-            child: SearchBar(),
+            child: Row(
+              children: [
+                Expanded(
+                  child: FocusScope(
+                    child: SearchBar(
+                      controller: widget.exploreController.searchTextController,
+                    ),
+                    onFocusChange: widget.exploreController.searchFocusChanged,
+                  ),
+                ),
+                SizeTransition(
+                  sizeFactor: CurvedAnimation(
+                    parent: iconAnimationController,
+                    curve: Curves.ease,
+                  ),
+                  axis: Axis.horizontal,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 16.sp,
+                    ),
+                    child: Icon(
+                      Icons.filter_alt_outlined,
+                      size: 24.sp,
+                      color: AppColors.primaryBlack,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           Expanded(
               child: ListView.separated(
