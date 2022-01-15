269c19c5f0fd618392e701ad06eada7ed240cda3
nayeemtby <nayeemtby@gmail.com>
2022-01-15 14:32:16 +0600

    Added more colors and txtthemes

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryWhite = Color(0xffffffff);
+  static const Color secondaryWhite = Color(0xb2FCFCFC);
+  static const Color tertiaryWhite = Color(0xffFFF9FF);
   static const Color primaryBlack = Color(0xff181725);
   static const Color primaryGrey = Color(0xff7C7C7C);
   static const Color secondaryGrey = Color(0xffE2E2E2);
+  static const Color tertiaryGrey = Color(0xffB1B1B1);
   static const Color accentGrey = Color(0xffF2F3F2);
   static const Color primaryGreen = Color(0xff53B175);
 }
--- lib/theme/txttheme.dart
+++ lib/theme/txttheme.dart
@@ -9,10 +9,22 @@ class TxtThemes {
     color: AppColors.primaryBlack,
   );
 
+  static final TextStyle headline2 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 26.sp,
+    color: AppColors.primaryBlack,
+  );
+
   static final TextStyle title = TextStyle(
     fontWeight: FontWeight.w700,
     fontSize: 16.sp,
     color: AppColors.primaryBlack,
+  );
+
+  static final TextStyle subtitle = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16.sp,
+    color: AppColors.primaryGrey,
   );
 
   static final TextStyle price = TextStyle(
@@ -24,6 +36,18 @@ class TxtThemes {
   static final TextStyle iconLabel = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 12.sp,
+    color: AppColors.primaryBlack,
+  );
+
+  static final TextStyle hint = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18.sp,
+    color: AppColors.tertiaryGrey,
+  );
+
+  static final TextStyle input = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18.sp,
     color: AppColors.primaryBlack,
   );
 }
