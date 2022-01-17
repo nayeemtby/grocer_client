d639044729f85d74004efe5be251d1a1fed4badc
nayeemtby <nayeemtby@gmail.com>
2022-01-17 17:53:42 +0600

    Added product counter UI component

--- lib/screens/components/input.dart
+++ lib/screens/components/input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grocer_client/theme/colors.dart';
 import 'package:grocer_client/theme/txttheme.dart';
@@ -31,6 +32,67 @@ class TxtInput extends StatelessWidget {
             color: AppColors.primaryGreen.withOpacity(0.7),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProductCounter extends StatelessWidget {
+  const ProductCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          Icons.remove,
+          size: 28.sp,
+        ),
+        SizedBox(
+          width: 20.w,
+        ),
+        IntrinsicWidth(child: _ProductCountField()),
+        SizedBox(
+          width: 20.w,
+        ),
+        Icon(
+          Icons.add,
+          size: 28.sp,
+        ),
+      ],
+    );
+  }
+}
+
+class _ProductCountField extends StatelessWidget {
+  const _ProductCountField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autocorrect: false,
+      inputFormatters: [
+        FilteringTextInputFormatter.digitsOnly,
+        FilteringTextInputFormatter.singleLineFormatter
+      ],
+      keyboardType: TextInputType.number,
+      maxLines: 1,
+      maxLength: 4,
+      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+      style: TxtThemes.input,
+      decoration: InputDecoration(
+        counterText: '',
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.r),
+          borderSide: BorderSide(width: 1.sp, color: AppColors.primaryGreen),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18.r),
+          borderSide: BorderSide(width: 1.sp, color: AppColors.secondaryGrey),
+        ),
+        contentPadding:
+            EdgeInsets.symmetric(vertical: 12.sp, horizontal: 18.sp),
       ),
     );
   }
