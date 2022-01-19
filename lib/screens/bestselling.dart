48c522326f192af92926151090c851f0f8e40a7a
nayeemtby <nayeemtby@gmail.com>
2022-01-19 23:37:30 +0600

    Added new screens

---
+++ lib/screens/bestselling.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:grocer_client/api/api.dart';
+import 'package:grocer_client/screens/components/product.dart';
+import 'package:grocer_client/theme/colors.dart';
+import 'package:grocer_client/theme/txttheme.dart';
+
+class BestsellingScr extends StatelessWidget {
+  const BestsellingScr({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+  final Map<String, dynamic> data;
+
+  @override
+  Widget build(BuildContext context) {
+    List tmp = data.values.toList();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.r,
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 24.r,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        size: 24.sp,
+                        color: AppColors.primaryBlack,
+                      ),
+                    ),
+                    Text(
+                      'Offers',
+                      style: TxtThemes.headline,
+                    ),
+                    Icon(
+                      Icons.filter_alt_outlined,
+                      size: 24.sp,
+                      color: AppColors.primaryBlack,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 6.sp,
+              ),
+              Expanded(
+                child: GridView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: tmp.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisExtent: 251.sp,
+                    crossAxisSpacing: 14.sp,
+                    mainAxisSpacing: 16.sp,
+                  ),
+                  itemBuilder: (ctx, index) {
+                    return ProductCard(
+                      price: tmp[index]['price'],
+                      quantity: tmp[index]['quantity'],
+                      title: tmp[index]['name'],
+                      url: baseUrl + tmp[index]['img'],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
